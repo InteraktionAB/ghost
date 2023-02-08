aa538234eb5d3fa1be7b24b26facb39e19f0297e
Sreerag M <sreeragibtl@gmail.com>
2023-02-08 21:14:23 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'GhostSwap',
+      version = '1.0',
+      description = "GhostSwap package",
+      author = "Sreerag",
+      author_email = "sreeragibtl@gmail.com",
+      install_requires = open('requirements.txt').read()
+     )
