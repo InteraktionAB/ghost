de0078cd01c5c8fa70b95dfd1920d9d8502c4296
Sreerag M <sreeragibtl@gmail.com>
2023-02-09 12:27:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from setuptools import setup
+import os
 
-setup(name = 'GhostSwap',
-      version = '1.0',
-      description = "GhostSwap package",
-      author = "Sreerag",
-      author_email = "sreeragibtl@gmail.com",
-      install_requires = open('requirements.txt').read()
-     )
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name="ghostswap",
+    py_modules=["AdaptiveWingLoss", "apex", "coordinate_reg", "examples", "insightface_func", "models", "network", "utils"],
+    version="1.0",
+    description="Ghost swap as API",
+    author="Sreerag",
+    url='https://github.com/InteraktionAB/ghost',
+    packages=find_packages(include=['AdaptiveWingLoss', 'apex', 'coordinate_reg', 'examples', 'insightface_func', 'models', 'network', 'utils']),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
+)
