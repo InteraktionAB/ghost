06ef70155cb114fc131d29bf9202dbf5b1e8b132
Sreerag M <sreeragibtl@gmail.com>
2023-02-14 17:53:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-inputs = {'image' : 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.gettyimages.com%2Fid%2F1293903541%2Fphoto%2Fyoung-woman-stock-photo.jpg',
+inputs = {'image' : 'https://thumbs.dreamstime.com/b/beauty-woman-face-portrait-beautiful-model-girl-perfect-fresh-clean-skin-spa-brunette-female-looking-camera-smiling-66363112.jpg',
         'video' : 'https://joy1.videvo.net/videvo_files/video/free/video0454/large_watermarked/_import_6064b7eaaff605.68960435_preview.mp4'}
 
 res = requests.post('http://localhost:8000/', json = inputs)
