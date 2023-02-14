7b5a791d9bcc6b2bfcd1dbc0ac2fa8aaac8c72c4
Sreerag M <sreeragibtl@gmail.com>
2023-02-14 18:08:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-inputs = {'image' : 'https://thumbs.dreamstime.com/b/beauty-woman-face-portrait-beautiful-model-girl-perfect-fresh-clean-skin-spa-brunette-female-looking-camera-smiling-66363112.jpg',
+inputs = {'image' : 'https://img.freepik.com/free-photo/front-view-man-with-nose-ring_23-2149441219.jpg',
         'video' : 'https://joy1.videvo.net/videvo_files/video/free/video0454/large_watermarked/_import_6064b7eaaff605.68960435_preview.mp4'}
 
 res = requests.post('http://localhost:8000/', json = inputs)
