5edebd616b1a3eab4a9b9f0fd2d5c87ef927bf0c
Sreerag M <sreeragibtl@gmail.com>
2023-02-21 16:19:05 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -71,7 +71,7 @@ class Predictor(BasePredictor):
     
     add_audio_from_another_video(source_video, OUT_VIDEO_NAME, "audio")
 
-    with open(OUT_VIDEO_NAME, 'rb') as videofile:
-        text = base64.b64encode(videofile.read()).decode('utf-8')
-    return text
+    #with open(OUT_VIDEO_NAME, 'rb') as videofile:
+        #text = base64.b64encode(videofile.read()).decode('utf-8')
+    return OUT_VIDEO_NAME
                    
